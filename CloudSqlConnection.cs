a152e82815f480d1bd50bb1fb15204c6ae5d65a7
Shawn Neal <sneal@sneal.net>
2016-11-18 00:15:08 +0000

    Add web.config connection string support

--- CloudSqlConnection.cs
+++ CloudSqlConnection.cs
@@ -1,43 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Web;
+using System.Web.Configuration;
 
 namespace ContosoUniversity
 {
     public class CloudSqlConnection
     {
-        private static String actualConnection = String.Empty;
+        private static string actualConnection = string.Empty;
 
-        public static String ConnectionString
+        public static string ConnectionString
         {
             get
             {
-                if (actualConnection == String.Empty)
+                if (string.IsNullOrEmpty(actualConnection))
                 {
-                    loadConnectionString();
+                    actualConnection = LoadConnectionString();
                 }
                 return actualConnection;
             }
         }
 
-        private static void loadConnectionString()
+        private static string LoadConnectionString()
         {
-            Dictionary<string, object> vcapServices = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(
-                Environment.GetEnvironmentVariable("VCAP_SERVICES"));
-            
-            if (vcapServices != null)
+            var vcapServicesRaw = Environment.GetEnvironmentVariable("VCAP_SERVICES");
+            if (string.IsNullOrEmpty(vcapServicesRaw))
             {
-                var credentials = ((Newtonsoft.Json.Linq.JArray)vcapServices["user-provided"]).First()["credentials"];
-                if (credentials != null)
-                {
-                    actualConnection = (String)credentials["connectionString"];
-                }
+                return WebConfigurationManager.ConnectionStrings["SchoolContext"].ConnectionString;
             }
 
-           // if (actualConnection == String.Empty) { 
-           //     actualConnection = "provide default string";
-          //  }                        
+            Dictionary<string, object> vcapServices = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(vcapServicesRaw);
+            var credentials = ((Newtonsoft.Json.Linq.JArray) vcapServices?["user-provided"])?.First()["credentials"];
+            if (credentials != null)
+            {
+                return (string)credentials["connectionString"];
+            }
+
+            throw new ConfigurationErrorsException(
+                "No Contoso SQL Server connection string was found. Did you forget to bind the connection string to the app?");                       
         }
     }
 }
