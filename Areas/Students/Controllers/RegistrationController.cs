37d2fd8d36ab49acd3cd72719f3da34851065438
Shawn Neal <sneal@sneal.net>
2019-04-25 09:12:03 -0700

    Add registration controller

---
+++ Areas/Students/Controllers/RegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ContosoUniversity.Areas.Students.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly SchoolContext _db = new SchoolContext();
+
+        // GET: Students/Registration
+        [Authorize(Roles = SchoolRole.Student)]
+        public ActionResult Index()
+        {
+            var principalID = User.Identity.GetUserId();
+            var student = _db.Students.SingleOrDefault(s => s.PrincipalID == principalID);
+            var enrollments = _db.Enrollments.Where(e => e.StudentID == student.ID);
+            return View(enrollments);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = SchoolRole.Student)]
+        public ActionResult Delete()
+        {
+            // TODO delete
+            return View("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = SchoolRole.Student)]
+        public ActionResult Register()
+        {
+            // TODO register/add class
+            return View("Index");
+        }
+    }
+}
