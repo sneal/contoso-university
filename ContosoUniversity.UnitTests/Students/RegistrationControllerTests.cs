c0e5c5feb119473a23521dddfcf5693232139fbd
Shawn Neal <sneal@sneal.net>
2019-04-29 10:18:34 -0700

    Add register test

--- ContosoUniversity.UnitTests/Students/RegistrationControllerTests.cs
+++ ContosoUniversity.UnitTests/Students/RegistrationControllerTests.cs
@@ -12,42 +12,45 @@ namespace ContosoUniversity.UnitTests
     public class RegistrationControllerTests
     {
         private readonly SchoolContext _schoolContext;
-        private readonly Student _student;
+        private readonly Student _currentStudent;
+        private readonly Course _algrebraCourse;
+        private readonly Department _mathDepartment;
 
         public RegistrationControllerTests()
         {
             var connection = Effort.DbConnectionFactory.CreateTransient();
             _schoolContext = new SchoolContext(connection);
 
-            var department = new Department
+            _mathDepartment = new Department
             {
                 Name = "Math",
                 StartDate = DateTime.Now,
                 Budget = 100m,
             };
-            _schoolContext.Departments.Add(department);
+            _schoolContext.Departments.Add(_mathDepartment);
 
-            var course = new Course
+            _algrebraCourse = new Course
             {
-                Department = department,
+                CourseID = 1,
+                Department = _mathDepartment,
                 Credits = 4,
                 Title = "Algebra"
             };
-            _schoolContext.Courses.Add(course);
+            _schoolContext.Courses.Add(_algrebraCourse);
 
-            _student = new Student
+            _currentStudent = new Student
             {
                 EnrollmentDate = DateTime.Now,
                 FirstMidName = "John",
                 LastName = "Smith",
                 PrincipalID = Guid.NewGuid().ToString()
             };
-            _schoolContext.Students.Add(_student);
+            _schoolContext.Students.Add(_currentStudent);
 
             var enrollment = new Enrollment
             {
-                Course = course,
-                Student = _student,
+                Course = _algrebraCourse,
+                Student = _currentStudent,
                 Grade = Grade.B
             };
             _schoolContext.Enrollments.Add(enrollment);
@@ -64,11 +67,10 @@ namespace ContosoUniversity.UnitTests
         }
 
         [Fact]
-        public void Index_shows_all_enrollments_for_current_user()
+        public void Index_shows_enrollment_for_current_student()
         {
-     
             var controller = new RegistrationController(_schoolContext);
-            controller.CurrentStudent = () => _student;
+            controller.CurrentStudent = () => _currentStudent;
 
             var view = (ViewResult) controller.Index();
             var enrollments = (IEnumerable<Enrollment>) view.Model;
@@ -77,6 +79,62 @@ namespace ContosoUniversity.UnitTests
             var en = enrollments.First();
             Assert.Equal("John", en.Student.FirstMidName);
             Assert.Equal("Algebra", en.Course.Title);
+        }
+
+        [Fact]
+        public void Index_does_not_show_enrollments_for_other_students()
+        {
+            var student2 = new Student
+            {
+                EnrollmentDate = DateTime.Now,
+                FirstMidName = "Maria",
+                LastName = "Rosa",
+                PrincipalID = Guid.NewGuid().ToString()
+            };
+            _schoolContext.Students.Add(_currentStudent);
+
+            var enrollment = new Enrollment
+            {
+                Course = _algrebraCourse,
+                Student = _currentStudent,
+                Grade = Grade.A
+            };
+            _schoolContext.Enrollments.Add(enrollment);
+            _schoolContext.SaveChanges();
+
+            var controller = new RegistrationController(_schoolContext);
+            controller.CurrentStudent = () => _currentStudent;
+
+            var view = (ViewResult)controller.Index();
+            var enrollments = (IEnumerable<Enrollment>)view.Model;
+
+            Assert.Single(enrollments);
+            Assert.Equal(_currentStudent, enrollments.First().Student);
+        }
+
+
+        [Fact]
+        public void Register_shows_all_courses()
+        {
+            var statisticsCourse = new Course
+            {
+                CourseID = 2,
+                Department = _mathDepartment,
+                Credits = 5,
+                Title = "Statistics"
+            };
+            _schoolContext.Courses.Add(statisticsCourse);
+            _schoolContext.SaveChanges();
+
+            var controller = new RegistrationController(_schoolContext);
+            controller.CurrentStudent = () => _currentStudent;
+
+            var view = (ViewResult)controller.Register();
+            var courses = (IEnumerable<Course>)view.Model;
+
+            Assert.Equal(2, courses.Count());
+            Assert.Contains(_algrebraCourse, courses);
+            Assert.Contains(statisticsCourse, courses);
         }
     }
 }
