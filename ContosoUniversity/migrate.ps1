b1ff6d469f4a83d9706f886721340c58a8661b06
Shawn Neal <sneal@sneal.net>
2019-04-30 14:11:36 -0700

    Add migration script

---
+++ ContosoUniversity/migrate.ps1
@@ -0,0 +1,5 @@
+﻿# Powershell script to run DB migrations
+$webConfig = "$PSScriptRoot\Web.config"
+$binDir = "$PSScriptRoot\bin"
+
+.\bin\migrate.exe ContosoUniversity.dll /startupConfigurationFile=$webConfig /StartUpDirectory=$binDir /verbose
